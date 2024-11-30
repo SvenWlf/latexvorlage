d3b1cbfdfa439bf474e349f6af84c9765952a38f
SvenWlf <sven-w-3@web.de>
2024-11-30 21:05:22 +0100

    initial commit

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -f *.fdb_latexmk *.run.xml *.synctex.gz *.aux *.log *.lof *.bak *.lot *.bbl *.blg *.dvi *.out *.brf *.thm *.toc *.idx *.ilg *.ind *.bcf *.equ *.fls
+
+echo "Auxiliary files deleted"
---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+latexmk -pdf -quiet Praxisarbeit.tex
+
+echo "Compiled Latex"
